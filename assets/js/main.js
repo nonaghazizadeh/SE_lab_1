dbb414e4ef251db595fbdae9c87d04ea1e08e524
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2023-03-09 18:30:52 +0330

    feat: add function for filtering task

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,4 +19,39 @@ Array.from(colors).forEach((col) => {
   });
 });
 
+document.querySelector('#filter-red').addEventListener("click", function () {
+    var tasks = document.querySelectorAll(".task .desc");
+    for (i = 0; i < tasks.length; i++){
+      tasks[i].style.display = "none";
+    }
+    for (i = 0; i < tasks.length; i++){
+      if (tasks[i].getAttribute("color") === "red") {
+        tasks[i].style.display = "block";
+      }
+    }
+  })
+  
+document.querySelector('#filter-green').addEventListener("click", function () {
+    var tasks = document.querySelectorAll(".task .desc");
+    for (i = 0; i < tasks.length; i++){
+    tasks[i].style.display = "none";
+    }
+    for (i = 0; i < tasks.length; i++){
+    if (tasks[i].getAttribute("color") === "green") {
+        tasks[i].style.display = "block";
+    }
+    }
+})
+  
+document.querySelector('#filter-blue').addEventListener("click", function () {
+    var tasks = document.querySelectorAll(".task .desc");
+    for (i = 0; i < tasks.length; i++){
+      tasks[i].style.display = "none";
+    }
+    for (i = 0; i < tasks.length; i++){
+      if (tasks[i].getAttribute("color") === "blue") {
+        tasks[i].style.display = "block";
+      }
+    }
+  })
 
